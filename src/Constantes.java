7f6279711318fc7ed936010754baaf02e87ccfbc
benzianedjazia <djaziadev4@gmail.com>
2023-10-13 18:55:55 +0200

    c1: ajout des fichiers

---
+++ src/Constantes.java
@@ -0,0 +1,4 @@
+public class Constantes {
+    public static final String HELLO_WORLD_TITLE = "Title Hello world";
+    public static final String HELLO_WORLD_TEXT = "Hello world text";
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Utilisation des constantes définies dans Constants.java
+        String title = Constants.HELLO_WORLD_TITLE;
+        String text = Constants.HELLO_WORLD_TEXT;
+
+        // Création de la fenêtre
+        JFrame fenetre = new JFrame(title);
+        fenetre.setSize(new Dimension(200, 200));
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Création du label de texte
+        JLabel label = new JLabel(text);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setForeground(Color.YELLOW);
+
+        // Finalisation
+        fenetre.add(label);
+        fenetre.setVisible(true);
+    }
+}
